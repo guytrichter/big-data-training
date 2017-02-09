f1372d95d472984097826184b668a07481879cce
Guy <guyt@feedvisor.com>
2017-02-09 18:07:30 +0200

    fix master carton not unique

--- gazuros/src/main/java/com/gazuros/inventory/controller/InventoryController.java
+++ gazuros/src/main/java/com/gazuros/inventory/controller/InventoryController.java
@@ -30,6 +30,7 @@ public class InventoryController {
     private static final String HERBS_KIT = "HERBS";
     private static final String CRAZY_GARDEN = "CRAZY_GARDEN";
     public static final int BOX_PRODUCT_ID = 72;
+    public static final String NOT_UNIQUE_TO_ANY_KIT = "Not unique to any kit";
 
     @Autowired
     private InventoryDao inventoryDao;
@@ -114,6 +115,7 @@ public class InventoryController {
 
                 Map<String, Collection<Pair<Long, Integer>>> kitsMap = kits.asMap();
                 boolean appearedInPrev = false;
+                boolean appearsInAnyKit = false;
                 for (String kitName : kitsMap.keySet()) {
                     Collection<Pair<Long, Integer>> values = kitsMap.get(kitName);
                     for (Pair<Long, Integer> val : values) {
@@ -121,7 +123,7 @@ public class InventoryController {
                         if (productId == inventoryResult.productId) {
 
                             if (appearedInPrev) {
-                                inventoryResult.kitName="Not unique to any kit";
+                                inventoryResult.kitName= NOT_UNIQUE_TO_ANY_KIT;
                             } else {
 
                                 //first kit this product is found in
@@ -129,11 +131,19 @@ public class InventoryController {
                                 inventoryResult.kitName = kitName;
                             }
 
+                            appearsInAnyKit = true;
                             result.add(inventoryResult);
                         } else {
 //                            System.out.println("Skip product");
                         }
                     }
+                }
+
+                if (!appearsInAnyKit) {
+
+                    //like master carton
+                    inventoryResult.kitName= NOT_UNIQUE_TO_ANY_KIT;
+                    result.add(inventoryResult);
                 }
 
             }
