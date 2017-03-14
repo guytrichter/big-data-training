5e9776bb13eb27cb58a2edd48f800053ea9d3041
Guy <guyt@feedvisor.com>
2017-03-14 09:01:01 +0200

    adding redis

--- reversefile/src/main/scala/com/recongate/App.scala
+++ reversefile/src/main/scala/com/recongate/App.scala
@@ -2,6 +2,7 @@ package com.recongate
 
 import akka.actor.{Actor, ActorSystem, Props}
 import akka.routing.RoundRobinPool
+import com.redis.{RedisClient, RedisClientPool, RedisClientPoolByAddress}
 
 import scala.io.Source
 
@@ -9,16 +10,24 @@ import scala.io.Source
  * @author Guy Trichter
  */
 object App {
-  
-  case object LineRequest
-  case object BeginProcessing
+
   case class Line(index : Integer, text : String)
 
-  class Worker extends Actor {
+
+  class Worker() extends Actor {
+
+    val redisClient : RedisClient = new RedisClient("localhost", 6379)
 
     def receive = {
       case Line(index, text) => {
-        println(index + ": " + text)
+        val reverseLine = text.reverse
+        redisClient.hset("reverseLines", index, reverseLine)
+        reverseLine.split(" ").foreach(word => {
+          val currentScore : Option[Double] = redisClient.zscore("popularWords", word)
+          val newScore = currentScore.getOrElse(0.0) + 1.0
+          println(word + ": " + newScore)
+          redisClient.zadd("popularWords", newScore, word)
+        })
       }
       case _ => {
         println("Received unknown message!")
@@ -29,15 +38,15 @@ object App {
   def main(args : Array[String]) {
 
     val system = ActorSystem("ReverseFile")
-    val worker = system.actorOf(Props(new Worker).withRouter(RoundRobinPool(10)))
+    val worker = system.actorOf(Props(new Worker()).withRouter(RoundRobinPool(10)))
 
-    var index : Integer = 0;
+    var index : Integer = 0
     Source.fromFile("/home/guy/backup").getLines.foreach(line => {
       worker ! Line(index, line)
-      index = index + 1
+      index += 1
     })
 
-    system.stop(worker)
+//    system.terminate
   }
 
 }
